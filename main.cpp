e95b0a80c8eda612e5b95ee3e2a6fa4a72bd198b
Vishesh Meena <visheshmeena47@gmail.com>
2025-03-27 18:30:21 +0530

    screen clear

--- main.cpp
+++ main.cpp
@@ -6,17 +6,19 @@ enum options{SHOW = 1, CREATE, DELETE, COMPLETE, INCOMPLETE, PRIORITY, EDIT};
 int main(){
     lists();
     int i = 0;
+    bool show = false;
     while(true){
         int option = get_option();
+        if(!show) std::cout << "\033[H\033[J";
         std::string quit;
         switch(option){
-            case SHOW: show_task(); break;
-            case CREATE: create_task(); break;
-            case DELETE: delete_task(); break;
-            case COMPLETE: mark_complete(); break;
-            case INCOMPLETE: mark_incomplete(); break;
-            case PRIORITY: set_priority(); break;
-            case EDIT: edit_task(); break;
+            case SHOW: show_task(); show = true; break;
+            case CREATE: create_task(); show = false; break;
+            case DELETE: delete_task(); show = false; break;
+            case COMPLETE: mark_complete(); show = false; break;
+            case INCOMPLETE: mark_incomplete(); show = false; break;
+            case PRIORITY: set_priority(); show = false; break;
+            case EDIT: edit_task(); show = false; break;
         }
         i++;
         save_list(); //saves lists every iterations
